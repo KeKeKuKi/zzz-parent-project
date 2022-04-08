b56c9535296189df1f07e55d76a1b8cc2a18d5ad
阿忠 <2669918628@qq.com>
2022-04-08 17:37:31 +0800

    nacos config

--- services/service-blog/src/test/java/CuratorTest.java
+++ services/service-blog/src/test/java/CuratorTest.java
@@ -1,12 +1,5 @@
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.zookeeper.CreateMode;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.annotation.RestController;
 import per.zzz.blog.BlogServerApplication;
-import per.zzz.zookeeper.CuratorService;
-
-import javax.annotation.Resource;
 
 /**
  * @author 阿忠 2669918628@qq.com
@@ -14,11 +7,11 @@ import javax.annotation.Resource;
  */
 @SpringBootTest(classes = BlogServerApplication.class)
 public class CuratorTest {
-    @Resource
-    public CuratorFramework curatorFramework;
-
-    @Test
-    public void testCurator() throws Exception {
-        System.out.println(curatorFramework.create().withMode(CreateMode.PERSISTENT).forPath("/node","data".getBytes()));
-    }
+//    @Resource
+//    public CuratorFramework curatorFramework;
+//
+//    @Test
+//    public void testCurator() throws Exception {
+//        System.out.println(curatorFramework.create().withMode(CreateMode.PERSISTENT).forPath("/node","data".getBytes()));
+//    }
 }
